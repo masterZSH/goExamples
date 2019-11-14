ee4f89ee271a192235b1f63d2d19f1e7aeba29e0
zsh <shaohua.zhou@joojtech.com>
2019-11-14 18:28:37 +0800

    map sort练习

--- map/kv/kv.go
+++ map/kv/kv.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -38,4 +39,53 @@ func main() {
 	for k, v := range week {
 		fmt.Printf("k=%d;v=%s\n", k, v)
 	}
+
+	var ma = map[string]int{"foo": 1, "bar": 2, "z": 3, "abc": 4}
+	sortMap(ma)
+
+	drink()
+}
+
+type name struct {
+	key   string
+	value int
+}
+
+type names []name
+
+func sortMap(m map[string]int) {
+	var n names = []name{}
+	for k, v := range m {
+		n = append(n, name{
+			key:   k,
+			value: v,
+		})
+	}
+	// sort.Strings(n)
+	fmt.Print(n)
+}
+
+// 构造一个将英文饮料名映射为法语（或者任意你的母语）的集合；
+// 先打印所有的饮料，然后打印原名和翻译后的名字。
+// 接下来按照英文名排序后再打印出来。
+func drink() {
+	var m = map[string]string{
+		"cola":  "可乐",
+		"water": "水",
+		"tea":   "茶",
+	}
+	for k, v := range m {
+		fmt.Printf("drank is %s\n", k)
+		fmt.Printf("%s,%s\n", k, v)
+	}
+	// sort
+	var s = []string{}
+	for k := range m {
+		s = append(s, k)
+	}
+	sort.Strings(s)
+	for _, v := range s {
+		fmt.Printf("%s,%s\n", v, m[v])
+	}
+
 }
