8af1178acccbf2b6dd218f7963f41d096361b7e5
zsh <shaohua.zhou@joojtech.com>
2019-11-25 16:21:01 +0800

    动态类型

---
+++ interface/dynamic/dynamic.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type xmlWriter interface {
+	WriteXML(w io.Writer)
+}
+
+type St struct {
+}
+
+func (s St) WriteXML(w io.Writer) {
+	fmt.Print(w)
+}
+
+func T(v interface{}) {
+	x, ok := v.(xmlWriter)
+	fmt.Print(x, ok)
+}
+
+func main() {
+	// test type
+	T(1)
+	// 显式申请类型更容易发现错误
+	var s xmlWriter = St{}
+	T(s)
+}
