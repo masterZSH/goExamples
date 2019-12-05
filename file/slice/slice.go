e602a76b8cae20fd99e2380e22f694082f40dea7
zsh <shaohua.zhou@joojtech.com>
2019-12-05 15:53:29 +0800

    file切片读取

---
+++ file/slice/slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "os"
+
+// Count byte count
+const Count int = 512
+
+func main() {
+	var bytes [Count]byte
+	f, _ := os.Open("test.text")
+	for {
+		i, _ := f.Read(bytes[:])
+		os.Stdout.Write(bytes[0:i])
+	}
+}
