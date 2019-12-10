708b01a7dca05b9b179e028b9efb2ce2e45a85ea
zsh <shaohua.zhou@joojtech.com>
2019-12-10 18:20:38 +0800

    chan block练习

---
+++ chan/block/block.go
@@ -0,0 +1,24 @@
+package main
+
+// 写一个通道证明它的阻塞性，开启一个协程接收通道的数据，持续 15 秒，
+// 然后给通道放入一个值。在不同的阶段打印消息并观察输出。
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+	go getData(ch)
+	go setData(ch)
+	time.Sleep(1e9)
+}
+
+func setData(ch chan string) {
+	ch <- "123"
+}
+
+func getData(ch chan string) {
+	fmt.Print(<-ch)
+	time.Sleep(19e9)
+}
