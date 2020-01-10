c64bb50ece92700dc6a781194525e6115cb738d5
zsh <shaohua.zhou@joojtech.com>
2020-01-10 17:56:23 +0800

    命令行输入

--- net/tcp/http/get/get.go
+++ net/tcp/http/get/get.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+var url string
+
+func init() {
+	flag.StringVar(&url, "u", "", "input url")
+}
+
 func main() {
 	// var url = "https://v2ex.com"
-	var errUrl = "https://vvvv.v2.co"
-	res, err := http.Get(errUrl)
+	// var errUrl = "https://vvvv.v2.co"
+	// if flag.NArg() < 1 {
+	// 	panic("参数错误")
+	// }
+	flag.Parse()
+	res, err := http.Get(url)
 	checkError(err)
 	data, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
 	checkError(err)
 	fmt.Print(string(data))
 }
 
 func checkError(err error) {
 	if err != nil {
-		log.Fatalf("err: %v", err)
+		log.Fatalf("er: %v", err)
 	}
 }
