bd1a61726c009d39d45c0edb37c5cf9b14dc7dca
zsh <shaohua.zhou@joojtech.com>
2020-01-10 14:12:10 +0800

    net/http包练习

--- net/tcp/http/webhello/webhello.go
+++ net/tcp/http/webhello/webhello.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/hello/", helloFunc)
+	http.HandleFunc("/shuthello/", shouthelloFunc)
 	http.ListenAndServe("localhost:9999", nil)
 }
 
 func helloFunc(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello "+req.URL.Path[len("/hello/"):])
+}
+
+func shouthelloFunc(w http.ResponseWriter, req *http.Request) {
+	res := req.URL.Path[len("/shuthello/"):]
+	res = strings.ToUpper(res)
+	fmt.Fprintf(w, "hello "+res)
 }
