b32524f0fd445675ecfa97a366febe950e7dddc2
zsh <shaohua.zhou@joojtech.com>
2020-01-13 19:09:59 +0800

    rpc调用练习

---
+++ rpc/rpc_client/go.mod
@@ -0,0 +1,7 @@
+module server
+
+go 1.13
+
+require rpc_objects v0.0.0
+
+replace rpc_objects v0.0.0 => ../rpc_objects
---
+++ rpc/rpc_client/rpc_client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"rpc_objects"
+	"log"
+	"net/rpc"
+	"fmt"
+)
+
+const serverAddress = "localhost"
+
+func main() {
+	client, err := rpc.DialHTTP("tcp", serverAddress + ":1234")
+	if err != nil {
+		log.Fatal("Error dialing:", err)
+	}
+	args := &rpc_objects.Args{7, 8}
+	var reply int
+	err = client.Call("Args.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("Args error:", err)
+	}
+	fmt.Printf("Args: %d * %d = %d", args.N, args.M, reply)	
+}
