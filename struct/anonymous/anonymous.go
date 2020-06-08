2fa1e923220f57ed31427ffeda4a325aaa9e7e95
zsh <shaohua.zhou@joojtech.com>
2020-06-08 16:55:35 +0800

    Add:匿名结构体变量

--- struct/anonymous/anonymous.go
+++ struct/anonymous/anonymous.go
@@ -11,7 +11,21 @@ type Stest struct {
 	int
 }
 
+// 匿名
+type a struct {
+	i, j int
+}
+
+type b struct {
+	a
+	k int
+}
+
 func main() {
 	var sT = &Stest{1.1, "2", 3}
 	fmt.Printf("%+v\n", sT)
+
+	var s2 b
+	s2.k = 2
+	fmt.Printf("%+v\n", s2)
 }
