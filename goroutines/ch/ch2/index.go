2b4ef3099d1b148f1a2dd6fb2fcfe2c41a362fa7
masterZSH <104438932@qq.com>
2020-07-07 21:57:22 +0800

    style:格式化

--- goroutines/ch/ch2/index.go
+++ goroutines/ch/ch2/index.go
@@ -8,16 +8,16 @@ import (
 
 func main() {
 	// 超时通道 超时时候写入该通道
-	timeout:=make(chan bool)
-	ch:=make(chan int,10)
+	timeout := make(chan bool)
+	ch := make(chan int, 10)
 	// 5s后超时写入超时通道
 	go timeoutFunc(timeout)
 	// 每隔1s发送数字到ch通道
 	go sendNum(ch)
-	for{
+	for {
 		select {
-		case num:=<-ch:
-			fmt.Printf("get num:%d\n",num)
+		case num := <-ch:
+			fmt.Printf("get num:%d\n", num)
 		case <-timeout:
 			// 超时 退出程序
 			fmt.Printf("timeout\n")
@@ -26,16 +26,16 @@ func main() {
 	}
 }
 
-func sendNum(ch chan int){
-	var i int 
-	for{
-		ch<-i
+func sendNum(ch chan int) {
+	var i int
+	for {
+		ch <- i
 		time.Sleep(time.Second)
 		i++
 	}
 }
 
-func timeoutFunc(timeout chan bool)  {
-	time.Sleep(5*time.Second)
-	timeout<-true
+func timeoutFunc(timeout chan bool) {
+	time.Sleep(5 * time.Second)
+	timeout <- true
 }
