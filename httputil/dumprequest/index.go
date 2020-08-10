6ecba40c7f78169ed1692bce39dc9aff5146790e
zsh <1044438932@qq.com>
2020-08-10 11:35:47 +0800

    feat:httputil dumprequest联系

---
+++ httputil/dumprequest/index.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+
+	// 创建一个http服务，构建处理请求方法
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		// 写入响应
+		fmt.Fprintf(w, "%q", dump)
+	}))
+	defer ts.Close()
+
+	// 请求body
+	const body = "Go is a general-purpose language designed with systems programming in mind."
+
+	// 创建一个请求  地址是上面http服务的地址
+	req, err := http.NewRequest("POST", ts.URL, strings.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Host = "www.example.org"
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// 读取响应
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", b)
+}
