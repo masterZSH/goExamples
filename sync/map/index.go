9d1d2d02380b22f3f0761882f2272fda52feec10
masterZSH <1044438932@qq.com>
2020-09-13 16:58:41 +0800

    add:添加普通map和sync.Map并发下比较

--- sync/map/index.go
+++ sync/map/index.go
@@ -7,7 +7,25 @@ import (
 
 // Map is like a Go map[interface{}]interface{}
 func main() {
-	m := new(sync.Map)
-	m.Store(1, 2)
-	fmt.Println(m.Load(1))
+	var m sync.Map
+	// m1 := make(map[int]string)
+
+	go func() {
+		for {
+			m.Store(1, "val")
+			// m1[1] = "val"
+		}
+	}()
+	go func() {
+		// 不停地对map进行读取
+		for {
+			// _ = m1[1]
+			fmt.Println(m.Load(1))
+		}
+	}()
+
+	for {
+
+	}
+
 }
