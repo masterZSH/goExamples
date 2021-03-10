765bd90bda279875a6ca591bc9fdd479dc001063
zsh <1044438932@qq.com>
2021-03-10 10:47:27 +0800

    add:bufio scanner

--- bufio/bufio.go
+++ bufio/bufio.go
@@ -3,14 +3,27 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	s:=strings.NewReader("1233")
-	var r *bufio.Reader
-	r = bufio.NewReader(s)
+	s := strings.NewReader("1233")
+	r := bufio.NewReader(s)
+	// r.Read([]byte{'1'})
+	fmt.Println(r.Buffered(), r.Size())
+
+	rs := bufio.NewReaderSize(s, 100)
+	rs.WriteTo(os.Stdout)
+
 	// 123
-	st,_:= r.Peek(3)
-	fmt.Printf("%s",st)
+	st, _ := r.Peek(3)
+	fmt.Printf("%s", st)
+
+	// scanner
+	rd := strings.NewReader("test commm")
+	scanner := bufio.NewScanner(rd)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
