003da77bb69b5283a306ea196c9f4fde4941ddb5
zsh <1044438932@qq.com>
2021-08-16 16:53:02 +0800

    add:错误处理

--- grpc/hclient-node/client.js
+++ grpc/hclient-node/client.js
@@ -1,9 +1,16 @@
+const process = require('process');
 var PROTO_PATH = __dirname + '/../pb/helloworld/helloworld.proto';
 console.log(PROTO_PATH);
 var parseArgs = require('minimist');
 var grpc = require('@grpc/grpc-js');
 var grpc_xds = require('@grpc/grpc-js-xds');
 grpc_xds.register();
+
+// handle exception
+process.on("uncaughtExpection", (err) => {
+    console.log(err);
+})
+
 
 var protoLoader = require('@grpc/proto-loader');
 var packageDefinition = protoLoader.loadSync(
@@ -20,6 +27,7 @@ function main() {
   var argv = parseArgs(process.argv.slice(2), {
     string: 'target'
   });
+  // 目标地址 grpc服务端
   var target;
   if (argv.target) {
     target = argv.target;
@@ -28,6 +36,8 @@ function main() {
   }
   var client = new hello_proto.Greeter(target,
                                        grpc.credentials.createInsecure());
+
+  // user 第一个参数传进来 默认值world
   var user;
   if (argv._.length > 0) {
     user = argv._[0]; 
