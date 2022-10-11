c941792b32fad2373cfbddc2a344592f361ce140
zsh <1044438932@qq.com>
2022-10-11 13:43:09 +0800

    add:conform trim

---
+++ byte/trim/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.19
+
+require github.com/leebenson/conform v1.2.2
+
+require github.com/etgryphon/stringUp v0.0.0-20121020160746-31534ccd8cac // indirect
--- byte/trim/main.go
+++ byte/trim/main.go
@@ -5,10 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
+
+	"github.com/leebenson/conform"
 )
 
 type Data struct {
 	Foo TrimSpaceString `json:"foo" binding:"required"`
+}
+
+type Data2 struct {
+	Foo string `json:"foo" binding:"required" conform:"trim"`
 }
 
 type TrimSpaceString string
@@ -25,4 +31,10 @@ func main() {
 	var d Data
 	json.Unmarshal([]byte(str), &d)
 	fmt.Println(d)
+
+	d2 := Data2{
+		Foo: "   b ar   ",
+	}
+	conform.Strings(&d2)
+	fmt.Println(d2)
 }
