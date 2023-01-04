9bcc6ddcd110c7a54db9fb155505c44d4b6b642a
zsh <1044438932@qq.com>
2023-01-04 14:24:12 +0800

    add:io multiwriter

---
+++ io/multiwriter/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("some io.Reader stream to be read\n")
+
+	var buf1, buf2 bytes.Buffer
+	w := io.MultiWriter(&buf1, &buf2)
+
+	if _, err := io.Copy(w, r); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print(buf1.String())
+	fmt.Print(buf2.String())
+
+}
