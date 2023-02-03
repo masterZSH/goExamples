2806d362caab88db28d46e391b3ad449c651d9b5
zsh <1044438932@qq.com>
2023-02-03 09:07:22 +0800

    os pipe

--- os/pipe/pipe.go
+++ os/pipe/pipe.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() {
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-	fmt.Println("sample")
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-	if len(out) == 0 {
-		fmt.Println("info log could not be written")
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("read %s\n", out)
+	_, err = w.Write([]byte("123"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 1024)
+	n, err := r.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", data[:n])
+	// rescueStdout := os.Stdout
+	// r, w, _ := os.Pipe()
+	// os.Stdout = w
+	// fmt.Println("sample")
+	// defer w.Close()
+	// out, _ := ioutil.ReadAll(r)
+	// os.Stdout = rescueStdout
+	// if len(out) == 0 {
+	// 	fmt.Println("info log could not be written")
+	// }
+	// fmt.Printf("read %s\n", out)
 }
