3271822649dc8d78d07b593ccaa66acf1764c36f
zsh <1044438932@qq.com>
2024-11-15 11:38:19 +0800

    add:reflect deepequal

--- reflect/deepequal/index.go
+++ reflect/deepequal/index.go
@@ -9,6 +9,10 @@ func main() {
 	a := (*HandleFunc)(nil)
 	b := (*HandleFunc)(nil)
 	fmt.Print(reflect.DeepEqual(a, b))
+
+	i1 := []int{1, 2, 3}
+	i2 := []int{1, 2, 3}
+	fmt.Println(reflect.DeepEqual(i1, i2))
 }
 
 type HandleFunc func()
